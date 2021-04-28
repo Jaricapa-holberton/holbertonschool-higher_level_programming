9c6dbb7e7e6382a543c7b18129479c2fa60b433b
Jaricapa-holberton <2241@holbertonschool.com>
2021-04-28 17:42:54 -0500

    task 3 v1 created

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv
+    argc = len(argv) - 1
+    sum = 0
+    i = 1
+    if argc <= 1:
+        print(sum)
+    else:
+        while i <= argc:
+            sum = sum + int(argv[i])
+            i = i + 1
+        print(sum)
+
+
+
+    
+        
+
+
