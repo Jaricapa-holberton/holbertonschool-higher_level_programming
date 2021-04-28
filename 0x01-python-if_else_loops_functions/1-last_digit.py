f21c2fdf6e1dc9f8ea3f9d16728e3b92564e1922
Jaricapa-holberton <2241@holbertonschool.com>
2021-04-27 20:07:03 -0500

    change permisions taks 0 and created task 1 v1

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+""" check the minimal settings """
+if number < 0:
+    last_number = (abs(number) % 10) * -1
+else:
+    last_number = number % 10
+print("Last digit of {:d} is {:d} and is".format(number, last_number), end=" ")
+""" check the what digit is the last number """
+if (last_number > 5):
+    print("and is greater than 5")
+elif (last_number < 6 and last_number != 0):
+    print("and is less than 6 and not 0")
+else:
+    print("and is 0")
