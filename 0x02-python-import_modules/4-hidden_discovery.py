4eb66c638a7b4d40ac89bffea821c97486c4b960
Jaricapa-holberton <2241@holbertonschool.com>
2021-04-28 18:12:50 -0500

    task 4 and 5 created

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    stings = dir(hidden_4)
+    for string in strings:
+        if string[:2] != "__":
+            print(string)
