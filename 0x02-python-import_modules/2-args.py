72574c3598951a1389a1b2c9de909ffa471a83d2
Jaricapa-holberton <2241@holbertonschool.com>
2021-04-28 17:30:09 -0500

    task 2 v5

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,13 +4,13 @@ if __name__ == "__main__":
     argv = sys.argv
     argc = len(argv)
     i = 1
-    if argc == 1:
+    if (argc == 1):
         print("{:d} arguments.".format(argc - 1))
     else:
-        if argc == 2:
-            print("{:d} argument.".format(argc - 1))
+        if (argc == 2):
+            print("{:d} argument:".format(argc - 1))
         else:
-            print("{:d} arguments.".format(argc - 1))
-        while i <= argc - 1:
+            print("{:d} arguments:".format(argc - 1))
+        while i <= (argc - 1):
             print("{:d}: {:s}".format(i, argv[i]))
             i += 1
