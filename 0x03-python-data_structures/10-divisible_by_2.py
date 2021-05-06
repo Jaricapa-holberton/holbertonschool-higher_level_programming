5065437cea7f9bcc212f961c44de18dd17fb01e9
Jaricapa-holberton <2241@holbertonschool.com>
2021-05-05 21:47:46 -0500

    task 10 v1 created

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    def evennum(n): 
+        if (n % 2 == 0):
+            return True
+        else:
+            return False
+    new_list = [evennum(i) for i in my_list]
+    return (new_list)
