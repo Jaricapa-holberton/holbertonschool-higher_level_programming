b0901aadcc3c68f257eca1e3f29ed1fa821305da
Jaricapa-holberton <2241@holbertonschool.com>
2021-05-17 14:17:27 -0500

    task 1 v1

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    """Prints an integer"""
+#    check_int = isinstance(value, int)
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except ValueError:
+        return (False)
