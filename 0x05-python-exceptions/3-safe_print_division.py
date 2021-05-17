27a8c1505c8bb2bd7c49bef536d521cba1f274c9
Jaricapa-holberton <2241@holbertonschool.com>
2021-05-17 15:40:55 -0500

    task 3 v2

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    """Divide 2 integers and prints the result"""
     result = 0
     try:
         result = a / b
-        print("Inside result: {:d}".format(result))
     except ZeroDivisionError:
         result = None
-        print("Inside result: {:s}".format(result))
     finally:
+        print("Inside result: {}".format(result))
         return (result)
