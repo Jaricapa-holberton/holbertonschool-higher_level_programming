6fbd906ccd330ee9fd10ccc1ef33736e989ef049
Jaricapa-holberton <2241@holbertonschool.com>
2021-05-18 18:22:15 -0500

    task 2 v1

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''
+Define a class: Square
+'''
+
+
+class Square:
+    '''A class to represent a square.'''
+    def __init__(self, size = 0):
+        '''
+        Construct all the necessary attributes for the square object.
+        '''
+
+        # Check if size is a correct input
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self._Square__size = size
