75ea8a296d0f5f599bf8a8ed912771dde8d4ef60
Jaricapa-holberton <2241@holbertonschool.com>
2021-05-18 21:41:01 -0500

    task 5 v1

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -19,9 +19,36 @@ class Square:
         else:
             self._Square__size = size
 
+    @property
+    def size(self):
+        '''Getter and setter of size.'''
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Setter of size."""
+
+        # Check if size is a correct input
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self._Square__size = value
+
     def area(self):
         '''
         returns the current square area.
         '''
 
         return (self._Square__size ** 2)
+
+    def my_print(self):
+        '''Print a square.'''
+
+        if self.__size == 0:
+            print()
+            return
+        else:
+            for i in range(self.__size):
+                print("{}".format("#" * self.__size))
