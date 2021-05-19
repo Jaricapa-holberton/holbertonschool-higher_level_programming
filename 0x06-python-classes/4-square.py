bed47fcdf49fc3018e0abdde1f6e14d8992c73e4
Jaricapa-holberton <2241@holbertonschool.com>
2021-05-18 21:36:37 -0500

    task 4 v1

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+'''
+Define a class: Square
+'''
+
+
+class Square:
+    '''A class to represent a square.'''
+    def __init__(self, size=0):
+        '''
+        Construct all the necessary attributes for the square object.
+        '''
+
+        # Check if size is a correct input
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self._Square__size = size
+
+    @property
+    def size(self):
+        '''Getter and setter of size.'''
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Setter of size."""
+
+        # Check if size is a correct input
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self._Square__size = value
+
+    def area(self):
+        '''
+        returns the current square area.
+        '''
+
+        return (self._Square__size ** 2)
