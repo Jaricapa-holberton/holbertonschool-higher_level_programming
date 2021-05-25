41d2606eacee1283e3c983e17cfa4f127d7e4890
Jaricapa-holberton <2241@holbertonschool.com>
2021-05-25 16:56:32 -0500

    task 101 v2

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -11,17 +11,48 @@ def lazy_matrix_mul(m_a, m_b):
     """
     import numpy as np
 
-    message_1 = "m_a must be a list"
-    message_2 = "m_b must be a list"
-    message_3 = "division by zero"
-    message_4 = "Each row of the matrix must have the same size"
-
+    message_1 = "new m_a must be a list"
+    message_2 = "new m_b must be a list"
+    message_3 = "new m_a must be a list of lists"
+    message_4 = "new m_b must be a list of lists"
+    message_5 = "new m_a can't be empty"
+    message_6 = "new m_b can't be empty"
+    message_7 = "new m_a should contain only integers or floats"
+    message_8 = "new m_b should contain only integers or floats"
+    message_9 = "new each row of m_a must be of the same size"
+    message_10 = "new each row of m_b must be of the same size"
+    message_11 = "new m_a and m_b can't be multiplied"
 
     if (isinstance(m_a, list) is not True):
-        raise TypeError(message_1)
-    for rows in matrix:
+        raise IndexError(message_1)
+    if (isinstance(m_b, list) is not True):
+        raise IndexError(message_2)
+    for rows in m_a:
         if (isinstance(rows, list) is not True):
-            raise TypeError(message_1)
+            raise IndexError(message_3)
+    for rows in m_b:
+        if (isinstance(rows, list) is not True):
+            raise IndexError(message_4)
+    if (m_a == [[]]):
+        raise IndexError(message_5)
+    if (m_b == [[]]):
+        raise IndexError(message_6)
+    elements = []
+    [elements.extend(i) for i in m_a]
+    if not all(type(num) in (int, float) for num in elements):
+        raise IndexError(message_7)
+    elements = []
+    [elements.extend(j) for j in m_b]
+    if not all(type(num) in (int, float) for num in elements):
+        raise IndexError(message_8)
+    for rows in m_a:
+        if (len(rows) != len(m_a[0])):
+            raise IndexError(message_9)
+    for rows in m_b:
+        if (len(rows) != len(m_b[0])):
+            raise IndexError(message_9)
+    if ((len(m_a) == 1) and (len(m_b) == 1)):
+        raise IndexError(message_11)
 
     A = np.array(m_a)
     B = np.array(m_b)
