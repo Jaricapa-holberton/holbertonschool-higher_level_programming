3113d91080419cb1ff1aaad19914bff5bb7d82c0
Jaricapa-holberton <2241@holbertonschool.com>
2021-05-25 14:22:14 -0500

    task 2 v2

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -12,27 +12,31 @@ def matrix_divided(matrix, div):
     Raises:
         TypeError: When a or b is a non-integer and non-float.
     """
-    if (isinstance(matrix, list) != True):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    message_1 = "matrix must be a matrix (list of lists) of integers/floats"
+    message_2 = "div must be a number"
+    message_3 = "division by zero"
+    message_4 = "Each row of the matrix must have the same size"
+    if (isinstance(matrix, list) is not True):
+        raise TypeError(message_1)
     for rows in matrix:
-        if (isinstance(rows, list) != True):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        if (isinstance(rows, list) is not True):
+            raise TypeError(message_1)
     elements = []
     [elements.extend(l) for l in matrix]
     if not all(type(num) in (int, float) for num in elements):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    if (matrix == None):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    if (div == None):
-        raise TypeError("div must be a number")
-    if ((matrix == None) and (div == None)):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    if (div == 0):
-        raise ZeroDivisionError("division by zero")
+        raise TypeError(message_1)
+    if (matrix is None):
+        raise TypeError(message_1)
+    if (div is None):
+        raise TypeError(message_2)
+    if ((matrix is None) and (div is None)):
+        raise TypeError(message_1)
+    if (div is 0):
+        raise ZeroDivisionError(message_3)
     for rows in matrix:
-        if ((len(rows)) != (len(matrix[0]))):
-            raise TypeError("Each row of the matrix must have the same size")
-    
+        if ((len(rows)) is not (len(matrix[0]))):
+            raise TypeError(message_4)
+
     new_matrix = []
     for l in matrix:
         list_2 = []
@@ -40,5 +44,5 @@ def matrix_divided(matrix, div):
             number = round(num / div, 2)
             list_2.append(number)
         new_matrix.append(list_2)
-    
+
     return new_matrix
