9d625b96e82483f552adbdd052e695fa9440b741
Jaricapa-holberton <2241@holbertonschool.com>
2021-06-02 07:40:47 -0500

    task 0 v2

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,7 +3,15 @@
 
 
 def read_file(filename=""):
-    """Read a text file with UTF-8 encoding and print out"""
+    """
+    Read a text file with UTF-8 encoding and print out
+
+    parametters:
+    *filename: a string of the file name
+
+    return:
+    *none
+    """
 
     with open(filename, mode="r", encoding="utf-8") as myfile:
         print(myfile.read(), end="")
