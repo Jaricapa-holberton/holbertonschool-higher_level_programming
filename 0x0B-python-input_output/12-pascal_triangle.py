840d217a942accae47464c107fec4e22df4755c8
Jaricapa-holberton <2241@holbertonschool.com>
2021-06-02 18:40:08 -0500

    task 12 v1

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,31 +1,36 @@
 #!/usr/bin/python3
-"""Deine class_to_json function"""
+"""Define class_to_json function"""
 
 
 def pascal_triangle(n):
     """
-    Return a list of lists of integers representing
-    the Pascal’s triangle of n
+    Do a list of lists of integers representing the Pascal’s The_triangle of n
+
+    parametters:
+    *n: a integrer number
+
+    return:
+    *returns a list of lists of integers
     """
 
-    triangle = []
+    The_triangle = []
 
     if n <= 0:
-        return triangle
+        return The_triangle
     if n == 1:
-        triangle.append([1])
-        return triangle
+        The_triangle.append([1])
+        return The_triangle
     if n == 2:
-        triangle.extend([[1], [1, 1]])
-        return triangle
+        The_triangle.extend([[1], [1, 1]])
+        return The_triangle
 
     if n > 2:
-        triangle.extend([[1], [1, 1]])
-        for n_row in range(3, n+1):
+        The_triangle.extend([[1], [1, 1]])
+        for new_Row in range(3, n+1):
             row = [1]
-            for i in range(n_row-3):
-                row.append(sum(triangle[-1][0 + i:2 + i]))
-            row.extend([n_row-1, 1])
-            triangle.append(row)
+            for i in range(new_Row-3):
+                row.append(sum(The_triangle[-1][0 + i:2 + i]))
+            row.extend([new_Row-1, 1])
+            The_triangle.append(row)
 
-    return triangle
+    return The_triangle
