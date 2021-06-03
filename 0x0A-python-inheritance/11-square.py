b48be69070e0e93ab41581cd4359fd611eb57d80
Jaricapa-holberton <2241@holbertonschool.com>
2021-06-02 21:41:04 -0500

    task 11 v1

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Define Square class"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represent a Square object"""
+
+    def __init__(self, size):
+        """
+        Inicialize a square object
+
+        parametters:
+        *self: the class itself
+        *size: the size of the square
+
+        return:
+        *none
+        """
+
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
