45ea15d701a75568028f3573850b4165b776f4ef
Jaricapa-holberton <2241@holbertonschool.com>
2021-06-02 21:09:48 -0500

    task 3 v1

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Define the is_same_class function"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    show True or False about if the object is an instance
+
+    parametters:
+    *obj: an Python object to use
+    *a_class: a class to use
+
+    return:
+    *True if instance or False if not
+    """
+
+    return isinstance(obj, a_class)
