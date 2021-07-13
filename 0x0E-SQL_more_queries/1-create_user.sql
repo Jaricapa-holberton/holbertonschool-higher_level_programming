d42251ea5fea19eb9b1797588a0200705ca3bb19
Jaricapa-holberton <2241@holbertonschool.com>
2021-07-13 17:45:13 -0500

    task 1 v3

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,9 +1,10 @@
 -- Creates the MySQL server user user_0d_1. Creates user_0d_1 should have all privileges on your MySQL server. The user_0d_1 password should be set to user_0d_1_pwd. If the user user_0d_1 already exists, your script should not fail
 -- when we grant some privileges for a user, running the command flush privileges will reloads the grant tables in the mysql database enabling the changes to take effect without reloading or restarting mysql service
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
-IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON * . *
-TO 'test'@'localhost'
-IDENTIFIED BY 'user_0d_1_pwd';
-
+CREATE USER
+    IF NOT EXISTS 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
+   ON *.*
+   TO 'user_0d_1'@'localhost'
+   IDENTIFIED BY 'user_0d_1_pwd';
 FLUSH PRIVILEGES;
