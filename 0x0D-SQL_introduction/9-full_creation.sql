625b8625fa620b6c6f8a98265a59592ff30ba00b
Jaricapa-holberton <2241@holbertonschool.com>
2021-07-12 19:11:48 -0500

    task 9 v1

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,18 @@
+-- Creates a table second_table in the database hbtn_0c_0 in the current database in my MySQL server.
+CREATE TABLE IF NOT EXISTS `first_table` (
+    `id` INT,
+    `name` VARCHAR(256)
+    `score` INT
+);
+
+INSERT INTO `first_table`(`id`,  `name`, `score`) 
+VALUES (1, "Jhon", 10);
+
+INSERT INTO `first_table`(`id`,  `name`, `score`) 
+VALUES (2, "Alex", 3);
+
+INSERT INTO `first_table`(`id`,  `name`, `score`) 
+VALUES (3, "Bob", 14);
+
+INSERT INTO `first_table`(`id`,  `name`, `score`) 
+VALUES (4, "George", 8);
