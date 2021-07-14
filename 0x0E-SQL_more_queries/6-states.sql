4a272e733315aa48e8c7ec49405f2b51378404ca
Jaricapa-holberton <2241@holbertonschool.com>
2021-07-14 06:42:09 -0500

    task 6 v2

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,9 @@
--- Creates the table unique_id on your MySQL server.
+-- Create the database hbtn_0d_usa and the table states on your MySQL server.
 -- The database name will be passed as an argument of the mysql command
 -- If the table unique_id already exists, your script should not fail
-CREATE TABLE IF NOT EXISTS `unique_id` (
-    `id` INT DEFAULT 1 UNIQUE,
-    `name` VARCHAR(256)
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
+    PRIMARY KEY(`id`),
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(256) NOT NULL
 );
