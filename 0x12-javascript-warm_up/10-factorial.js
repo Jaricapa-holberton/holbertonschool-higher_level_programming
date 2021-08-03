c187234fc99a36776eb2427df213c2b57e6b24fe
Jaime Aricapa <2241@holbertonschool.com>
2021-08-02 20:01:35 -0500

    task 9 v2

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const num = parseInt(process.argv[2]);
+
+function factorial (n) {
+  if (isNaN(n) || n === 0) {
+    return (1);
+  } else {
+    return (n * (factorial(n - 1)));
+  }
+}
+
+console.log(factorial(num));
