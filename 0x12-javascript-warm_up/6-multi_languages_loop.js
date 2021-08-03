e6021a3e1e3c68ff15696e74dc094e67b661e75a
Jaime Aricapa <2241@holbertonschool.com>
2021-08-02 19:14:15 -0500

    task 6 v1

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const strlist = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const lenlist = strlist.length;
+let i;
+
+for (i = 0; i < lenlist; i++) {
+  console.log(strlist[i]);
+}
