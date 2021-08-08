1688a2cfc9a1927c52b0d685653ca9eeda7d5ab7
Jaime Aricapa <2241@holbertonschool.com>
2021-08-08 15:45:20 -0500

    task 11 v1

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Add the State object “Louisiana” to the database hbtn_0e_6_usa
+"""
+from sys import argv
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+
+
+if __name__ == "__main__":
+    """
+    Print the State object with the name passed as argument
+    from the database hbtn_0e_6_usa
+    """
+    # Create engine. The args comes from input
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    # Start Session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    # Query
+    new = State(name="Louisiana")
+    states = session.add(new)
+    session.commit()
+    # Print results
+    print(new.id)
