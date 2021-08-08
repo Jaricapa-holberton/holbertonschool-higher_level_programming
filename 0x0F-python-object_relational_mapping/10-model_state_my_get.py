f5f0f7666488dbf6c7797addb2ccec8c850448d1
Jaime Aricapa <2241@holbertonschool.com>
2021-08-08 12:24:23 -0500

    task 10 v3

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -8,6 +8,7 @@ from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 from model_state import State
 
+
 # Add a security layer with the main function
 def main():
     """
