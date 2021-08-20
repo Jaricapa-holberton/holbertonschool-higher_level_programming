f6ce459d7d5a6c45a3c9a8f46fa9279f985f87ea
Jaime Aricapa <2241@holbertonschool.com>
2021-08-19 19:12:09 -0500

    update task 0 and 1

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Displays the size of the body content of indicated URL.
-# go to the url direction, search for content and bring just the count
+# takes in a URL, sends a request to that URL, and displays the size of the body of the response
+# Displays the size of the body content of indicated URL. go to the url direction, search for content and bring just the count
 curl -sI "$1" | grep -i "Content-Length:" | cut -d " " -f2
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # takes in a URL, sends a GET request to the URL, and displays the body of the response
 # Displays the body of the response from a indicated URL
-curl -sL "$1"
+curl -sL GET "$1"
