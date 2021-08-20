582eedfdb220696b3321440dce85fd8de7fc25da
Jaime Aricapa <2241@holbertonschool.com>
2021-08-19 19:00:19 -0500

    task 0 v

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# takes in a URL, sends a request to that URL, and displays the size of the body of the response
 # Displays the size of the body content of indicated URL.
 # go to the url direction, search for content and bring just the count
 curl -sI "$1" | grep -i "Content-Length:" | cut -d " " -f2
