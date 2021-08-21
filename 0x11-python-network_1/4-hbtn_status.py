03db875c6fbdd8780d22f550d6aadb8c2728b092
Jaime Aricapa <2241@holbertonschool.com>
2021-08-21 16:24:03 -0500

    task 4 v1

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""script that fetches https://intranet.hbtn.io/status"""
+if __name__ == "__main__":    
+    import requests
+
+    # get the data from the web page
+    req = requests.get('https://intranet.hbtn.io/status').text
+    # print the text
+    print("Body response:")
+    print("\t- type: {}".format(type(req)))
+    print("\t- content: {}".format(req))
