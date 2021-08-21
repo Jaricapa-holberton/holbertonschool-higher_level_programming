f0dc90a43689cc63a3c462293ac6a3cd2bb85289
Jaime Aricapa <2241@holbertonschool.com>
2021-08-21 14:31:15 -0500

    task 0 v2

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 """script that fetches https://intranet.hbtn.io/status"""
+if __name__ == "__main__":
+    import urllib.request
 
-import urllib.request
-
-# get the data from the web page
-ses_url = 'https://intranet.hbtn.io/status'
-with urllib.request.urlopen(ses_url) as response:
-    # save the html code fetched
-    html = response.read()
-# print the text as main demands
-print(html)
-print("Body response:")
-print("\t- type: {}".format(type(html)))
-print("\t- content: {}".format(html))
-print("\t- utf8 content: {}".format(html.decode('utf8')))
+    # get the data from the web page
+    ses_url = 'https://intranet.hbtn.io/status'
+    with urllib.request.urlopen(ses_url) as response:
+        # save the html code fetched
+        html = response.read()
+    # print the text as main demands
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html.decode('utf8')))
